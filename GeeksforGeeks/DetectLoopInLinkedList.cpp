b710b534b9d49f3bc769220197b4704d53b84446
abhishek13yadv <36642139+abhishek13yadv@users.noreply.github.com>
2020-02-07 17:17:05 +0530

    Create DetectLoopInLinkedList.cpp

---
+++ GeeksforGeeks/DetectLoopInLinkedList.cpp
@@ -0,0 +1,19 @@
+int detectloop(Node *head) {
+    
+    // your code here
+    Node* slow = head;
+    Node* fast = head;
+    while(slow && fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow==fast)
+        {
+            
+            return 1;
+        }
+    }
+    
+    return 0;
+    
+}
