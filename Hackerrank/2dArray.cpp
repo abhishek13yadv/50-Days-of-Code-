955051400fd13baddff20b51d6ab4058baf47d4e
abhishek13yadv <36642139+abhishek13yadv@users.noreply.github.com>
2020-02-24 17:42:56 +0530

    Create 2dArray.cpp

---
+++ Hackerrank/2dArray.cpp
@@ -0,0 +1,22 @@
+int hourglassSum(vector<vector<int>> arr) {
+    int max_sum = INT_MIN;
+    for(int i=0;i<6;i++)
+    {
+        for(int j=0;j<6;j++)
+        {
+            if (i > 1 && j > 1) {
+                    int sum = arr[i][j]
+                            + arr[i][j - 1]
+                            + arr[i][j - 2]
+                            + arr[i - 1][j - 1]
+                            + arr[i - 2][j]
+                            + arr[i - 2][j - 1]
+                            + arr[i - 2][j - 2];
+
+                    if (sum > max_sum)
+                        max_sum = sum;
+            }
+        }
+    }
+    return max_sum;
+}
