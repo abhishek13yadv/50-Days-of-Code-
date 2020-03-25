3f257d3d09b34530812582b8ce41ae09b3086ce5
abhishek13yadv <36642139+abhishek13yadv@users.noreply.github.com>
2020-03-25 22:55:25 +0530

    Add files via upload

---
+++ GeeksforGeeks/lcmArr.cpp
@@ -0,0 +1,34 @@
+// C++ program to find LCM of n elements 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+typedef long long int ll; 
+
+int gcd(int a, int b) 
+{ 
+	if (b == 0) 
+		return a; 
+	return gcd(b, a % b); 
+} 
+ll findlcm(int arr[], int n) 
+{ 
+	ll ans = arr[0]; 
+
+	for (int i = 1; i < n; i++) 
+		ans = (((arr[i] * ans)) / (gcd(arr[i], ans))); 
+	return ans; 
+} 
+
+int main() 
+{ 
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	} 
+	cout<<findlcm(arr, n); 
+	return 0; 
+} 
+
