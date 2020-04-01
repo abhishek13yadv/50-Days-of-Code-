4e20da488555d27725d879cc7bb7062debe5b3ac
abhishek13yadv <36642139+abhishek13yadv@users.noreply.github.com>
2020-04-02 01:05:32 +0530

    Create stocks are profitable.cpp

---
+++ codezen/arrays/stocks are profitable.cpp
@@ -0,0 +1,16 @@
+int maxProfit(int *arr,int l)
+{
+//Write code here 
+    int min=arr[0], max=arr[0], profit=0;
+	for(int i=1;i<l;i++){
+    if(min>arr[i]){
+        min = arr[i];
+        max=arr[i];
+    }
+    if(max<arr[i])
+        max=arr[i];
+    if(profit<max-min)
+        profit=max-min;
+	}
+	return profit;
+}
